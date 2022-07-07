3b943394b5bc381a2edd091e03aaebba231a1489
Rei Kawai <91781515+kawaiGitHub@users.noreply.github.com>
2022-07-07 16:28:22 +0900

    Add files via upload

---
+++ Hamburger/script.js
@@ -0,0 +1,12 @@
+let nav = document.querySelector("#navArea")
+let bnt = document.querySelector(".toggle-btn")
+let mask = document.querySelector("#mask")
+
+bnt.onclick = () => {
+    nav.classList.toggle("open")
+};
+
+
+mask.onclick = () => {
+    nav.classList.toggle("open")
+};
